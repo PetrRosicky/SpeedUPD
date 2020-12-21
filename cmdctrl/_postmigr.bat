9fb5da88b1c1dd17c2bc304998271e41a81f2d4f
PetrRosicky <76439843+PetrRosicky@users.noreply.github.com>
2020-12-21 16:58:08 +0100

    Delete _postmigr.bat

--- cmdctrl/_postmigr.bat
+++
@@ -1,8 +0,0 @@
-del C:\smh_installer.log
-del "C:\Users\Public\Desktop\HPE System Management Homepage.lnk"
-
-set devmgr_show_nonpresent_devices=1
-start devmgmt.msc
-
-
-rem netsh interface ip reset "c:\ipreslog.txt"
