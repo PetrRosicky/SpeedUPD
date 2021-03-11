0c806017337a5bd241675c8c864864e0c4d7ae50
PetrRosicky <76439843+PetrRosicky@users.noreply.github.com>
2021-03-11 13:56:22 +0100

    Add files via upload

--- _cmdctrl/noHWsp/generalize.bat
+++ _cmdctrl/noHWsp/generalize.bat
@@ -2,9 +2,10 @@ mkdir c:\hp
 copy unatt.xml C:\hp\unatt.xml
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo Are you sure?
+echo Pressing any key will initiate Windows Hardware Installation process during next boot!
+echo System shutdown will be initiated once setup completed.
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 pause
 
 C:\Windows\System32\Sysprep\sysprep.exe /oobe /shutdown /generalize /unattend:C:\hp\unatt.xml
-
 
